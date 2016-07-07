fa1629e05b2e71d863e94c4c2cf498ea7817fc1f
asus pc <asus pc@asus>
2016-07-07 23:17:56 +0200

    choose day of week

---
+++ HomeTask/src/Task07.java
@@ -0,0 +1,30 @@
+
+public class Task07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int day = 5;
+String dayString;
+switch (day) {
+case 1: dayString = "Понедельник";
+break;
+case 2: dayString = "Вторник";
+break;
+case 3: dayString = "Среда";
+break;
+case 4: dayString = "Четверг";
+break;
+case 5: dayString = "Пятница";
+break;
+case 6: dayString = "Суббота";
+break;
+case 7: dayString = "Воскресенье";
+break;
+default: dayString = "Ошибка";
+break;
+}
+System.out.print(dayString);
+}
+	}
+
+
