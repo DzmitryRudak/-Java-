cd92c8ea52d9ad257d3da14d8453fe0eb163b51f
asus pc <asus pc@asus>
2016-07-07 23:00:32 +0200

    TaskFactorial( for example - num. 4)

---
+++ HomeTask/src/TaskFactorial.java
@@ -0,0 +1,14 @@
+
+public class TaskFactorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n = 4;
+int result = 1;
+for (int i = 1; i <= n; i ++){
+	result *= i;
+}
+System.out.print(result);
+	}
+
+}
